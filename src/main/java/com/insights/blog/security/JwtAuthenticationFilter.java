acb726257ff8a87b3a3004585519629fbb84bdba
harknoel <bontilao.harknoel@gmail.com>
2024-04-06 05:37:25 +0800

    Modify JwtAuthenticationFilter to verify if the user has authenticated.

--- src/main/java/com/insights/blog/security/JwtAuthenticationFilter.java
+++ src/main/java/com/insights/blog/security/JwtAuthenticationFilter.java
@@ -6,6 +6,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -17,11 +20,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
 
+    private final UserDetailsService userDetailsService;
+
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
         final String authorizationHeader = request.getHeader("Authorization");
         final String token;
-        final String email;
+        final String userEmail;
 
         if(authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
             filterChain.doFilter(request, response);
@@ -29,6 +34,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
 
         token = authorizationHeader.substring(7);
-        email = jwtService.extractUsername(token);
+        userEmail = jwtService.extractUsername(token);
+
+        if(userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+        }
     }
 }
