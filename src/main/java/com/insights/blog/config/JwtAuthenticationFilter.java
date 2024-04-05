e68757be76ea4fbcb1c78e35abaa5d2166f2e67b
harknoel <bontilao.harknoel@gmail.com>
2024-04-06 03:45:33 +0800

    Add JwtAuthenticationFilter class

---
+++ src/main/java/com/insights/blog/config/JwtAuthenticationFilter.java
@@ -0,0 +1,34 @@
+package com.insights.blog.config;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull FilterChain filterChain) throws ServletException, IOException {
+        final String authorizationHeader = request.getHeader("Authorization");
+        final String token;
+        final String email;
+
+        if(authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        token = authorizationHeader.substring(7);
+        email = jwtService.extractUser(token);
+    }
+}
