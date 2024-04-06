f5aaa81c9ee6c42ea47222b42b57f7dd05ee7409
harknoel <bontilao.harknoel@gmail.com>
2024-04-07 00:01:18 +0800

    Add jakarta dependency and Update RegisterRequestDTO

--- src/main/java/com/insights/blog/payload/RegisterRequestDTO.java
+++ src/main/java/com/insights/blog/payload/RegisterRequestDTO.java
@@ -1,5 +1,8 @@
 package com.insights.blog.payload;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,8 +13,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequestDTO {
+
+    @NotBlank
     private String firstname;
+
+    @NotBlank
     private String lastname;
+
+    @NotBlank
+    @Email
     private String email;
+
+    @NotBlank
     private String password;
 }
