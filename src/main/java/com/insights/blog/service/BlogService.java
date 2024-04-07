5380e2bd580e0f8f6895c62a5a115e74691a7b3f
harknoel <bontilao.harknoel@gmail.com>
2024-04-08 06:20:01 +0800

    Add BlogService class

--- src/main/java/com/insights/blog/service/BlogService.java
+++ src/main/java/com/insights/blog/service/BlogService.java
@@ -1,21 +1,48 @@
 package com.insights.blog.service;
 
 import com.insights.blog.entity.Blog;
+import com.insights.blog.entity.User;
+import com.insights.blog.payload.BlogRequestDTO;
+import com.insights.blog.payload.BlogResponseDTO;
 import com.insights.blog.repository.BlogRepository;
+import com.insights.blog.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class BlogService {
 
-    BlogRepository blogRepository;
+    private final BlogRepository blogRepository;
+
+    private final UserRepository userRepository;
 
     public ResponseEntity<List<Blog>> getAllPosts() {
         return new ResponseEntity<>(blogRepository.findAll(), HttpStatus.OK);
+    }
+
+    public BlogResponseDTO addBlog(BlogRequestDTO blogRequestDTO) {
+        Optional<User> requestUser = userRepository.findByEmail(blogRequestDTO.getEmail());
+        if(requestUser.isEmpty()) {
+            System.out.println(blogRequestDTO.getEmail());
+            // TO DO throw exception
+            return null;
+        }
+
+        User user = requestUser.get();
+        var blog = Blog
+                .builder()
+                .title(blogRequestDTO.getTitle())
+                .content(blogRequestDTO.getContent())
+                .user(user)
+                .build();
+        blogRepository.save(blog);
+
+        return new BlogResponseDTO(blog.getBlogId(), blog.getTitle(), blog.getContent(), blog.getDate(), blog.getComments());
     }
 }
