966d73e0140865da70270d82211fc522870f08c8
harknoel <bontilao.harknoel@gmail.com>
2024-04-08 06:18:15 +0800

    Add BlogResponseDTO

--- src/main/java/com/insights/blog/payload/BlogResponseDTO.java
+++ src/main/java/com/insights/blog/payload/BlogResponseDTO.java
@@ -1,2 +1,25 @@
-package com.insights.blog.payload;public class BlogResponseDTO {
+package com.insights.blog.payload;
+
+import com.insights.blog.entity.Comment;
+import com.insights.blog.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class BlogResponseDTO {
+    private Integer blogId;
+
+    private String title;
+
+    private String content;
+
+    private String date;
+
 }
