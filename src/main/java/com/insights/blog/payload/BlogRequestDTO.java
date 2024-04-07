19e1bd6b67e7fcc728f3c81ac62bbf1ed614573a
harknoel <bontilao.harknoel@gmail.com>
2024-04-08 06:18:34 +0800

    Add BlogRequestDTO

--- src/main/java/com/insights/blog/payload/BlogRequestDTO.java
+++ src/main/java/com/insights/blog/payload/BlogRequestDTO.java
@@ -1,10 +1,13 @@
 package com.insights.blog.payload;
 
+import com.insights.blog.entity.User;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class BlogRequestDTO {
+    @NotBlank
+    private String email;
 
     @NotBlank
     private String title;
