eeff8752959cc8a4555f246e776459d1ae0a8600
harknoel <bontilao.harknoel@gmail.com>
2024-04-09 00:24:50 +0800

    Add WebMvcConfig

---
+++ src/main/java/com/insights/blog/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.insights.blog.config;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Adjust this path according to your API endpoints
+                .allowedOrigins("http://localhost:3000") // Allow requests from this origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allow these HTTP methods
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization"); // Expose Authorization header to client
+    }
+}
