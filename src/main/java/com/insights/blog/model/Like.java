c9d5186b96400dd33f2a620a1458ed42ea50565e
harknoel <bontilao.harknoel@gmail.com>
2024-04-27 22:07:57 +0800

    Add like model

---
+++ src/main/java/com/insights/blog/model/Like.java
@@ -0,0 +1,32 @@
+package com.insights.blog.model;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_like")
+@EntityListeners(AuditingEntityListener.class)
+public class Like {
+    @Id
+    @GeneratedValue
+    private Integer likeId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonManagedReference
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "blog_id")
+    @JsonManagedReference
+    private Blog blog;
+}
