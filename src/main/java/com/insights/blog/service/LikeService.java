e2db6021557f8763a25162513ff373d07640ed06
harknoel <bontilao.harknoel@gmail.com>
2024-04-27 22:07:42 +0800

    Add like service

---
+++ src/main/java/com/insights/blog/service/LikeService.java
@@ -0,0 +1,9 @@
+package com.insights.blog.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LikeService {
+}
