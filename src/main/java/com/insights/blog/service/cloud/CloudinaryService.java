24fce9c1cfe3723bcc3faa1b0738dc9919cb3ccf
Topitops69 <topitops123@gmail.com>
2024-05-19 22:13:20 +0800

    Implemented the DELETE image from CLOUDINARY and Localhost

--- src/main/java/com/insights/blog/service/cloud/CloudinaryService.java
+++ src/main/java/com/insights/blog/service/cloud/CloudinaryService.java
@@ -1,7 +1,17 @@
 package com.insights.blog.service.cloud;
 
+import com.cloudinary.api.ApiResponse;
+import com.cloudinary.utils.ObjectUtils;
+import io.jsonwebtoken.lang.Maps;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 
 public interface CloudinaryService {
     public String uploadFile(MultipartFile file, String folderName);
+
+
+    public ApiResponse deleteResources(List<String> publicIds) throws Exception;
 }
