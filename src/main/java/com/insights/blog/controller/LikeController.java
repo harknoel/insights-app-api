fd95192c19ac72fa5b2a462fa3797ba9c77b13f2
herald-noel <bontilao.heraldnoel@gmail.com>
2024-05-19 14:42:08 +0800

    Refactor like implementation

--- src/main/java/com/insights/blog/controller/LikeController.java
+++ src/main/java/com/insights/blog/controller/LikeController.java
@@ -3,12 +3,10 @@ package com.insights.blog.controller;
 import com.insights.blog.model.Blog;
 import com.insights.blog.model.User;
 import com.insights.blog.payload.LikeRequestDTO;
+import com.insights.blog.security.CurrentUser;
 import com.insights.blog.service.LikeService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/likes")
@@ -17,8 +15,8 @@ public class LikeController {
 
     private LikeService likeService;
 
-    @PostMapping("/blog")
-    public boolean like(@RequestBody LikeRequestDTO likeRequestDTO) {
-        return likeService.likeBlog(likeRequestDTO.getUserId(), likeRequestDTO.getBlogId());
+    @PostMapping("/blog/{id}")
+    public boolean like(@PathVariable int id, @CurrentUser User user) {
+        return likeService.likeBlog(user, id);
     }
 }
--- src/main/java/com/insights/blog/service/LikeService.java
+++ src/main/java/com/insights/blog/service/LikeService.java
@@ -1,39 +1,26 @@
 package com.insights.blog.service;
 
-import com.insights.blog.exception.UserAndBlogNotFoundException;
 import com.insights.blog.model.Blog;
 import com.insights.blog.model.Like;
 import com.insights.blog.model.User;
 import com.insights.blog.repository.BlogRepository;
 import com.insights.blog.repository.LikeRepository;
-import com.insights.blog.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class LikeService {
 
     private final LikeRepository likeRepository;
-    private final UserRepository userRepository;
     private final BlogRepository blogRepository;
 
     public boolean userHasLikePost(User user, Blog blog) {
         return likeRepository.existsByUserAndBlog(user, blog);
     }
 
-    public boolean likeBlog(Integer userId, Integer blogId) {
-        Optional<User> userOptional = userRepository.findById(userId);
-        Optional<Blog> blogOptional = blogRepository.findById(blogId);
-
-        if (userOptional.isEmpty() && blogOptional.isEmpty()) {
-            throw new UserAndBlogNotFoundException(userId, blogId);
-        }
-
-        User user = userOptional.get();
-        Blog blog = blogOptional.get();
+    public boolean likeBlog(User user, Integer blogId) {
+        Blog blog = blogRepository.findById(blogId).orElseThrow();
 
         if (!userHasLikePost(user, blog)) {
             var like = Like.builder()
@@ -43,7 +30,6 @@ public class LikeService {
             likeRepository.save(like);
             return true;
         }
-
         return false;
     }
 }
