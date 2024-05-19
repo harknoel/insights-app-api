1d782333dadc19dc2127f0858a12662d5855294d
Christopher O. Castro <86904029+ChristopherCastro69@users.noreply.github.com>
2024-05-19 13:01:30 +0800

    Implemented the GET image endpoint (#8)
    * Fixed the AddBlog Service Image on userID
    
    * Will implement the Get Image now
    
    * Implemented the GET image endpoint

--- src/main/java/com/insights/blog/model/Image.java
+++ src/main/java/com/insights/blog/model/Image.java
@@ -1,5 +1,6 @@
 package com.insights.blog.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -26,9 +27,17 @@ public class Image {
 
     @ManyToOne
     @JoinColumn(name = "blog_id")
-    @JsonManagedReference
+    @JsonBackReference
     private Blog blog;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    @JsonManagedReference
+    private User user;
+
 
     @Column(name = "url_image")
     private String imageURL;
+
+
 }
